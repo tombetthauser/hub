b8696b9b8609ede9c281e666fa5f9395b6dab839
Tom Betthauser <tombetthauser@gmail.com>
2018-08-17 13:22:05 -0700

    added b&w color picker

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var t = (Math.random() * 600) + 100;
+var t = (Math.random() * 100) + 50;
 var a = ["http://www.tombetthauser.com/numbers", "http://www.tombetthauser.com/sad"];
 var l = (a.length) - 1;
 var x = Math.round(Math.random() * l);
 var ca = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+var nca = ["0","1","2","3","4","5","6"];
 var cal = ca.length - 1;
+var ncal = nca.length - 1;
 var cs = "#";
+var ker = Math.random()
+var colorChance = .1;
 
-colors();
-
-setTimeout(function(){
-	window.location = a[x];
-},t)
+if(ker <= colorChance){
+	colors();
+} else {
+	notColors();
+}
 
 function colors(){
 	setInterval(function(){
@@ -19,6 +23,16 @@ function colors(){
 		cp();
 		document.getElementById("bod").style.backgroundColor = cs;
 		colors();
+	}, ctf());
+}
+
+function notColors(){
+	setInterval(function(){
+		cs = "#";
+		ctf();
+		ncp();
+		document.getElementById("bod").style.backgroundColor = cs;
+		notColors();
 	}, ctf());
 }
 
@@ -32,5 +46,14 @@ function cp(){
 	for(i=0;i<6;i++){
 		var y = Math.round(Math.random() * cal)
 		cs += ca[y];
+	}
+}
+
+function ncp(){
+	var y = Math.round(Math.random() * ncal)
+	var z = Math.round(Math.random() * ncal)
+	for(i=0;i<3;i++){
+		cs += nca[y];
+		cs += nca[z];
 	}
 }
